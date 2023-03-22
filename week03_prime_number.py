4ef54d6b13a1b6ef99236ba239361ca9f9eaafd3
kang4303 <kanghm0602@gmail.com>
2023-03-22 20:43:11 +0900

    week03 prime number v0.3

--- week03_prime_number.py
+++ week03_prime_number.py
@@ -1,12 +1,10 @@
-# week03_prime_number v0.2
+# week03_prime_number v0.3
 number = int(input("Input number : "))
-count = 0
-
+is_prime = True
 for i in range(2, number): # -2 loop
     if number % i == 0:
-        count = count + 1
-
-if count == 0:
+        is_prime = False
+if is_prime == True:
     print(f"{number} is prime number!")
 else:
     print(f"{number} is NOT prime number.")
